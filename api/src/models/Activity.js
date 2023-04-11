ca7ef3ab2308f6e611f36d5ba772aa68107759a4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-04-10 22:18:48 -0300

    Bdd

---
+++ api/src/models/Activity.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Activity", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    // -  Dificultad (número del 1 al 5). \*
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    // -  Duración (en horas).
+    duration: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 0,
+      },
+    },
+    // -  Temporada (Verano, Otoño, Invierno o Primavera). \*
+    season: {
+      type: DataTypes.ENUM("Verano", "Otoño", "Invierno", "Primavera"),
+      allowNull: false,
+    },
+  });
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define("Country", {
+    //-  ID (Código de tres letras).
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    //-  Nombre. \*
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    //-  Imagen de la bandera. \*
+    flags: { type: DataTypes.STRING, allowNull: false },
+    //-  Continente. \*
+    region: { type: DataTypes.STRING, allowNull: false },
+    //-  Capital. \*
+    capital: { type: DataTypes.STRING },
+    //-  Subregión.
+    subregion: { type: DataTypes.STRING },
+    //-  Área.
+    area: { type: DataTypes.INTEGER },
+    //-  Población. \* */
+    population: { type: DataTypes.INTEGER, allowNull: false },
   });
 };
